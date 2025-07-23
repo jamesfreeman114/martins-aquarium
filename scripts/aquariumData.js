adc5cc0811ec47c52361b0c40817e54c8fba9c3d
James Freeman <jamesfreeman114@gmail.com>
2025-07-22 20:36:22 -0500

    Created database in aquariumData module

---
+++ scripts/aquariumData.js
@@ -0,0 +1,60 @@
+export const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/nemo.jpg"
+        },
+        {
+            name: "Dory",
+            species: "Blue Tang",
+            length: 5,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/dory.jpg"
+        },
+        {
+            name: "Angelica",
+            species: "Queen Angelfish",
+            length: 4,
+            location: "Bermuda Triangle",
+            diet: "Omnivore",
+            image: "https://www.marinefishez.com/images/stories/virtuemart/product/shutterstock_queen-angel7.jpg"
+        },
+        {
+            name: "Squawks",
+            species: "Parrotfish",
+            length: 6,
+            location: "Great Barrier Reef",
+            diet: "Herbivore",
+            image: "https://www.barrierreef.org/generated/1280w-16-9/127053-56-jpg.jpg?1619571139"
+        },
+        {
+            name: "Zippy",
+            species: "Cardinalfish",
+            length: 2,
+            location: "Banggai Archipelago",
+            diet: "Carnivore",
+            image: "https://reefcasa.com/wp-content/uploads/2022/11/Bangaii-Cardinalfish.jpg"
+        },
+        {
+            name: "Big Boi",
+            species: "Pufferfish",
+            length: 5,
+            location: "Mediterranean Sea",
+            diet: "Carnivore",
+            image: "https://www.ultimatepapermache.com/wp-content/uploads/2021/01/fb1d107297140ce9e0cb20d5d414271f.jpg"
+        },
+        {
+            name: "Bitey McBiteFace",
+            species: "Anglerfish",
+            length: 4,
+            location: "Mariana Trench",
+            diet: "Carnivore",
+            image: "https://c02.purpledshub.com/uploads/sites/62/2025/02/Humpback-anglerfish-Melanocetus-johnsonii-1.jpeg?w=750&webp=1"
+        },
+    ]
+}
