f1080b0024755e24f9a2e7f92812d328f93a1724
James Freeman <jamesfreeman114@gmail.com>
2025-07-26 14:02:55 -0500

    Updated locationList HTML and CSS

--- scripts/locationList.js
+++ scripts/locationList.js
@@ -5,11 +5,31 @@ export const locationList = () => {
 
     for (const location of database.locations) {
         locationHTML += `
-         <article class ="locations">
-         <section class="location">${location.name}: ${location.country}, ${location.description}</section>
-         </article>`;
+        <article class="locations">
+               <section class="location">
+                   <h2 class="location__name">${location.name}</h2>
+                   <h3 class="location___country">${location.country}</h3>
+                   <p class="location__description">${location.description}</p>
+               </section>
+           </article>`;
 
     }
     return locationHTML
 }
 
+//MORE ADVANCED LOCATIONS HTML
+
+{/* <article class="locations">
+               <div class="location__details">
+                   <h2 class="location__name">${location.name}</h2>
+                   <h3 class="location___country">${location.country}</h3>
+                   <p class="location__description">${location.description}</p>
+               </div>
+           </article>
+           ` */}
+
+//BASIC LOCATIONS HTML
+
+//    <article class ="locations">
+//  <section class="location">${location.name}: ${location.country}, ${location.description}</section>
+//  </article>
